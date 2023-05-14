67269713b3e0dcd089a67d1173f2aff782abab05
frankjmclarke <frankjmclarke@gmail.com>
2023-05-14 01:00:12 -0700

    Initial commit

---
+++ test/src/mocks.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:starter/src/features/authentication/data/firebase_auth_repository.dart';
+import 'package:starter/src/features/onboarding/data/onboarding_repository.dart';
+
+class MockAuthRepository extends Mock implements AuthRepository {}
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockUserCredential extends Mock implements UserCredential {}
+
+class MockUser extends Mock implements User {}
+
+class MockOnboardingRepository extends Mock implements OnboardingRepository {}
+
+class Listener<T> extends Mock {
+  void call(T? previous, T? next);
+}
